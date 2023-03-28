1813d110a3395d8ae2576e1e3bb0dce907c4e788
Guilherme Takeshi Oshiro Andrade <126925552+guilhermetakeshi@users.noreply.github.com>
2023-03-28 15:52:11 -0300

    Update sec4 aula179 Funções recursivas, recursividade e Stack Overflow.py

--- sec4 aula179 Funções recursivas, recursividade e Stack Overflow.py
+++ sec4 aula179 Funções recursivas, recursividade e Stack Overflow.py
@@ -1,11 +1,16 @@
 # Funções recursivas e recursividade
 # - funções que podem se chamar de volta
 # - úteis p/ dividir problemas grandes em partes menores
+
 # Toda função recursiva deve ter:
 # - Um problema que possa ser dividido em partes menores
+# - Tem que ser um caso onde se chame de volta (recursividade)
 # - Um caso recursivo que resolve o pequeno problema
 # - Um caso base que para a recursão
 # - fatorial - n! = 5! = 5 * 4 * 3 * 2 * 1 = 120
+
+# Stack Overflow acontece quando o call Stack fica cheio de tarefas a serem executadas, onde
+# o sistema quebra como forma de segurança para não acumular mta coisa
 
 def recursiva(inicio=0, fim=4):
 
