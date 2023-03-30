eaeab7d204175811b9125def09235f104bd4bc2a
Guilherme Takeshi Oshiro Andrade <126925552+guilhermetakeshi@users.noreply.github.com>
2023-03-30 11:09:57 -0300

    Add files via upload

---
+++ sec4 aula185 Criando e usando um requirements.py
@@ -0,0 +1,11 @@
+# pip uninstall nome_pacote
+# Congelar (ver pacotes)
+# pip freeze - só funciona 
+#
+# Criando e usando um requirements.txt
+# pip freeze > requirements.txt
+# isso criaria um TXT com todos os pacotes instalados que vc usou no projeto, assim, qnd vc 
+# for colocar em outro local, vc já sabe quais pacotes vc instalou e usou
+
+# Instalando tudo do requirements.txt
+# pip install -r requirements.txt
