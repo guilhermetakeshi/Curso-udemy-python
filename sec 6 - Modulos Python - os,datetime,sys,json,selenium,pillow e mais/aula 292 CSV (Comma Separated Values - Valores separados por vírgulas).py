f5be882e47c03d793e7cbf41129a14748bcc51f9
Guilherme Takeshi Oshiro Andrade <126925552+guilhermetakeshi@users.noreply.github.com>
2023-09-25 20:34:09 -0300

    Add files via upload

---
+++ sec 6 - Modulos Python - os,datetime,sys,json,selenium,pillow e mais/aula 292 CSV (Comma Separated Values - Valores separados por vírgulas).py
@@ -0,0 +1,87 @@
+# CSV (Comma Separated Values - Valores separados por vírgulas)
+# É um formato de arquivo que armazena dados em forma de tabela, onde cada
+# linha representa uma linha da tabela e as colunas são separadas por vírgulas.
+
+# Ele é amplamente utilizado para transferir dados entre sistemas de diferentes
+# plataformas, como por exemplo, para importar ou exportar dados para uma
+# planilha (Google Sheets, Excel, LibreOffice Calc) ou para uma base de dados.
+
+# Um arquivo CSV geralmente tem a extensão ".csv" e pode ser aberto em um
+# editor de texto ou em uma planilha eletrônica.
+
+# Um exemplo de um arquivo CSV pode ser:
+# Nome,Idade,Endereço
+# Luiz Otávio,32,"Av Brasil, 21, Centro"
+# João da Silva,55,"Rua 22, 44, Nova Era"
+
+# A primeira linha do arquivo define os nomes das colunas da, enquanto as
+# linhas seguintes contêm os valores das linhas, separados por vírgulas.
+
+# Regras simples do CSV
+# 1 - Separe os valores das colunas com um delimitador único (,)
+# 2 - Cada registro deve estar em uma linha
+# 3 - Não deixar linhas ou espaços sobrando
+# 4 - Use o caractere de escape (") quando o delimitador aparecer no valor.
+
+# csv.reader e csv.DictReader
+# csv.reader lê o CSV em formato de lista
+# csv.DictReader lê o CSV em formato de dicionário
+
+import csv
+from pathlib import Path
+
+# Define o caminho do arquivo CSV usando a biblioteca pathlib.
+CAMINHO_CSV = Path(__file__).parent / 'aula 292.csv'
+
+# Abre o arquivo CSV em modo de leitura ('r') usando um gerenciador de contexto (with).
+with open(CAMINHO_CSV, 'r') as arquivo:
+    # Cria um leitor CSV que lê o arquivo como um dicionário, onde as chaves são os cabeçalhos das colunas.
+    leitor = csv.DictReader(arquivo)
+
+    # Itera sobre as linhas do arquivo CSV e imprime o Nome, Idade e Endereço de cada linha.
+    for linha in leitor:
+        print(linha['Nome'], linha['Idade'], linha['Endereço'])
+
+# Lista de dicionários que representa os clientes a serem escritos no arquivo CSV.
+lista_clientes = [
+    {'Nome': 'Luiz Otávio', 'Endereço': 'Av 1, 22'},
+    {'Nome': 'João Silva', 'Endereço': 'R. 2, "1"'},
+    {'Nome': 'Maria Sol', 'Endereço': 'Av B, 3A'},
+]
+
+# Abre o arquivo CSV em modo de escrita ('w') usando um gerenciador de contexto (with).
+with open(CAMINHO_CSV, 'w') as arquivo:
+    # Obtém os nomes das colunas a partir do primeiro dicionário na lista de clientes.
+    nome_colunas = lista_clientes[0].keys()
+
+    # Cria um escritor CSV que escreverá os dados como um dicionário, usando os nomes das colunas como cabeçalhos.
+    escritor = csv.DictWriter(
+        arquivo,
+        fieldnames=nome_colunas
+    )
+
+    # Escreve o cabeçalho no arquivo CSV.
+    escritor.writeheader()
+
+    # Itera sobre a lista de clientes e escreve cada cliente no arquivo CSV.
+    for cliente in lista_clientes:
+        escritor.writerow(cliente)
+
+
+
+# lista_clientes = [
+#     ['Luiz Otávio', 'Av 1, 22'],
+#     ['João Silva', 'R. 2, "1"'],
+#     ['Maria Sol', 'Av B, 3A'],
+# ]
+# with open(CAMINHO_CSV, 'w') as arquivo:
+#     # nome_colunas = lista_clientes[0].keys()
+#     nome_colunas = ['Nome', 'Endereço']
+#     escritor = csv.writer(arquivo)
+
+#     escritor.writerow(nome_colunas)
+
+#     for cliente in lista_clientes:
+#         escritor.writerow(cliente)
+
+
